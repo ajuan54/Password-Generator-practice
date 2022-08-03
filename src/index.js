8f529ff26c31bc0faac13302a4315e7a8ce36e96
ajuan54 <97407823+ajuan54@users.noreply.github.com>
2022-08-02 20:27:15 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,127 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/"
+];
+
+let box1El = document.getElementById("box1");
+let box2El = document.getElementById("box2");
+
+function generatePassword1() {
+  const passwordAr = [];
+
+  for (let i = 0; i <= 1; i++) {
+    let password = "";
+    for (let j = 0; j <= 14; j++) {
+      let randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+    passwordAr.push(password);
+  }
+  return (box1El.textContent = passwordAr[0]);
+}
+function generatePassword2() {
+  const passwordAr = [];
+
+  for (let i = 0; i <= 1; i++) {
+    let password = "";
+    for (let j = 0; j <= 14; j++) {
+      let randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+    passwordAr.push(password);
+  }
+  return (box2El.textContent = passwordAr[1]);
+}
+password.addEventListener("click", function () {
+  generatePassword1();
+  generatePassword2();
+});
